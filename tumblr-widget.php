b1b1081becc673d318f9896eae3a091b6b970a40
gabrielroth <gabrielroth@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-29 10:56:22 +0000

    Forgot to update version number ...
    git-svn-id: https://plugins.svn.wordpress.org/tumblr-widget-for-wordpress/trunk@316451 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tumblr-widget.php
+++ tumblr-widget.php
@@ -3,7 +3,7 @@
  * Plugin Name: Tumblr Widget
  * Plugin URI: http://gabrielroth.com/tumblr-widget-for-wordpress/
  * Description: Displays a Tumblr on a WordPress page.
- * Version: 1.4.1
+ * Version: 1.4.2
  * Author: Gabriel Roth
  * Author URI: http://gabrielroth.com
  */
